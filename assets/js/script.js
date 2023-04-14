048bc2aeb1bfd99fc5deedc57f89e5a6c2c1e0b5
Phelipe Afonso <phelipeafonso.freitas@gmail.com>
2023-04-14 07:50:17 -0300

    Adicionando efeitos de transição btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,21 @@
 document.getElementById('btnAdd').addEventListener('click', function(event) {
-    event.stopPropagation();
-    var elementos = document.querySelectorAll('.btn--hide');
-    for (var i = 0; i < elementos.length; i++) {
-      elementos[i].classList.add('btn--show');
-      elementos[i].classList.remove('btn--hide');
-    }
+  event.stopPropagation();
+  var elementos = document.querySelectorAll('.btn--hide');
+  for (var i = 0; i < elementos.length; i++) {
+    elementos[i].classList.add('btn--show');
+    elementos[i].classList.remove('btn--hide');
+  }
   
-    document.addEventListener('click', function onClick(event) {
-      event.stopPropagation();
-      if (!event.target.closest('.btnGroup')) {
-        var elementos = document.querySelectorAll('.btn--show');
-        for (var i = 0; i < elementos.length; i++) {
-          elementos[i].classList.add('btn--hide');
-          elementos[i].classList.remove('btn--show');
-        }
-        document.body.classList.remove('btn--show');
-        document.removeEventListener('click', onClick);
+  function onClick(event) {
+    if (!event.target.closest('.btn--show')) {
+      var elementos = document.querySelectorAll('.btn--show');
+      for (var i = 0; i < elementos.length; i++) {
+        elementos[i].classList.remove('btn--show');
+        elementos[i].classList.add('btn--hide');
       }
-    });
-  });
+      document.removeEventListener('click', onClick);
+    }
+  }
+  
+  document.addEventListener('click', onClick);
+});
